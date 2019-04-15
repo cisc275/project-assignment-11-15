1eff104523896a46a4b6be725e72555b228841dd
Amelia Abobo <aabobo@udel.edu>
2019-04-14 23:05:57 -0400

    TopView to SideScroll

---
+++ Game/src/Files/SideScrollModel.java
@@ -0,0 +1,42 @@
+package Files;
+
+
+public class SideScrollModel{
+	int x;
+	int y;
+	int xchange;
+	int ychange;
+	static int motionPicNum;
+	static int actionPicNum;
+	final int xincr = 10;
+	final int yincr = 10;
+	int AnimalWidth;
+	int AnimalHeight;
+	int frameWidth;
+	int frameHeight;
+	String action;
+	Direction dir;
+	
+
+	
+	/**
+	 * Updates the location and direction of objects and characters. Will add more methods as needed that will be called by this one.
+	 *
+	 * @author
+	 * @param
+	 * @param
+	 * @param   
+	 * @return 
+	 * 
+	 * */;
+	void updateLocationAndDirection() {
+		//System.out.println("updating");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Starting point");
+		Controller ctrl = new Controller();
+		ctrl.start();
+	}
+}
+
