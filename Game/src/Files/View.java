e02870df2c783065ebebcf2af9e8e5c0893616d5
ahallak <ahallak@udel.edu>
2019-04-15 18:20:18 -0400

    colors

--- Game/src/Files/View.java
+++ Game/src/Files/View.java
@@ -77,9 +77,9 @@ public class View extends JPanel{
 	}
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(Color.RED);
-		g.drawRect(Model.clapperRail.getX(), -1*Model.clapperRail.getY(), 50, 50); //getX(), getY()
 		g.setColor(Color.BLUE);
+		g.drawRect(Model.clapperRail.getX(), -1*Model.clapperRail.getY(), 50, 50); //getX(), getY()
+		g.setColor(Color.RED);
 		g.fillRect(100, 100, 20, 20); //getX(), getY()
 	}
 	
